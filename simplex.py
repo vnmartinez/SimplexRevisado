dd8c09d8f8621e67ed5aa6f6925759208ebf9fcc
hnsaavedraa <hnsaavedraa@unal.edu.co>
2019-11-12 17:45:39 -0500

    add read input.txt

--- simplex.py
+++ simplex.py
@@ -1,15 +1,35 @@
 import numpy as np
 import sys
 
-problem = np.array([[1, 1, 0, 1, 1],
-                    [-2, 1, 4, 0, 2], 
-                    [-1, -2, 0, 0 ,1],
-            
-                    ])  
+
 
 # Obtener columnas problem[:,#columna]
 # Obtener fila problem[#fila, :]
 
+
+# Leemos el arhivo de entrada para crear asi la matriz con los coficientes del problema
+def readInput(inputName):
+    arr = []
+    f = open (inputName,"r")
+    f1 = f.readlines()
+    contador = 0
+    for x in f1:
+        arr.append([])
+        contador = contador +1
+    row = 0
+    for x in f1:
+        y=0
+        while y < len(x):    
+            if(x[y] == "-"):
+                string =int(x[y]+x[y+1])
+                arr[row].append(string)
+                y = y+1   
+            elif(x[y] != "-" and x[y] != " " and x[y] != "\n"):
+                arr[row].append(int(x[y]))
+            y = y +1            
+        row = row + 1
+    return arr
+      
 # Este metodo determinara en que columnas se encuentran las variables en forma canonica factible
 # En caso de no encontrarse en FCF se le notificara al usuario y se terminara la ejecucion del programa 
 # Retornara un arreglo indicando cuales son nuestras variables basicas iniciales
@@ -43,7 +63,8 @@ def crearB(A,B_aux):
     for x in range(0,len(B_aux)):
         B = (np.insert(B, B.shape[1], A[: , B_aux[x]], 1))
     B = B[:,len(B_aux):B.shape[1]]
-    print("B = ",B)
+    print("B = ")
+    print (B)
     return B
 #En este metodo se declara el vector c sub B
 def crearc_B(c,B_aux):
@@ -124,81 +145,87 @@ def createP_s(a_rs,A_s,col):
 def createP(P_s,minFila): 
      result = np.identity(len(P_s))
      result[:, minFila] = P_s
-     print("P = ",result)
+     print("P = ")
+     print(result)
      return result
 
 # se calcula la inversa de B
 def createB_inverse(B_inverse,P):
     result = np.dot(P,B_inverse)
-    print("Inversa de B = ",result)
+    print("Inversa de B = ")
+    print(result)
     return result
 
-
-## Primera iteracion 
-print(problem)
-A = problem[0:(problem.shape[0] - 1):, 0:(problem.shape[1] - 1)].copy() #Matriz A 
-print("A = ", A)
-b = problem[0:(problem.shape[0] - 1):,(problem.shape[1] - 1)].copy().reshape(problem.shape[0] - 1,1) # Vector b (coeficientes)
-print("b = ",b)
-c = problem[problem.shape[0] - 1,: problem.shape[1] - 1 ].copy() # Vector c (valores de z)
-print("c = ",c)
-B_aux = calcularFCF(problem) #Vector B_0
-#B_aux = [3, 2]
-print("B = ", B_aux)
-B = crearB(A,B_aux)
-B_inverse =np.identity(problem.shape[0]-1)
-""" B_inverse = np.array([[1, 0,],
-                      [0, 0.25],   
-                    ])  """
-
-
-c_B = crearc_B(c,B_aux)
-pi_z = np.dot(c_B,B_inverse)
-print("pi_z = ",pi_z)
-c_barra = c - (np.dot(pi_z,A))
-print("c _barra = ",c_barra)
-print("")
-
-numciclo = 1
-control = True
-
-##Ahora ejecutaremos ciclos hasta que todos los c_j sean mayores a ceros
-## o encontremos que no existe una SBF
-while(not comprobar(c_barra) and control):
-#while(numciclo < 10):
-    print("")
-    print("Ciclo numero ",numciclo)
-    
-    ColumnaPivote = minCol(c_barra)
-    A_sBarra =np.dot(B_inverse,A[:, ColumnaPivote] )
-    FilaPivote = minFila(b,A_sBarra,minCol)
-    if(FilaPivote == "error"):
-        break
-    B_aux = replaceB(B_aux,ColumnaPivote,FilaPivote)
+def main():
+    problem = np.array(readInput("input.txt")) 
+    ## Primera iteracion 
+    print(problem)
+    A = problem[0:(problem.shape[0] - 1):, 0:(problem.shape[1] - 1)].copy() #Matriz A 
+    print("A = ")
+    print(A)
+    b = problem[0:(problem.shape[0] - 1):,(problem.shape[1] - 1)].copy().reshape(problem.shape[0] - 1,1) # Vector b (coeficientes)
+    print("b = ")
+    print(b)
+    c = problem[problem.shape[0] - 1,: problem.shape[1] - 1 ].copy() # Vector c (valores de z)
+    print("c = ",c)
+    #B_aux = calcularFCF(problem) #Vector B_0
+    B_aux = [3, 2]
+    print("B = ", B_aux)
     B = crearB(A,B_aux)
-    a_rs = A[FilaPivote][ColumnaPivote]
-    print("a_rs = ",a_rs)
-    P_s = createP_s(a_rs,A_sBarra,FilaPivote)
-    P = createP(P_s,FilaPivote)
-    B_inverse = createB_inverse(B_inverse,P)
+    #B_inverse =np.identity(problem.shape[0]-1)
+    B_inverse = np.array([[1, 0,],
+                        [0, 0.25],   
+                        ]) 
+
+
     c_B = crearc_B(c,B_aux)
     pi_z = np.dot(c_B,B_inverse)
     print("pi_z = ",pi_z)
     c_barra = c - (np.dot(pi_z,A))
-    print("c_barra = ",c_barra)
-    numciclo = numciclo+1
-    print("")
+    print("c _barra = ",c_barra)
     print("")
 
-# cumplida la condicion encontraremos la SBF
-if(control == True):
-    print("Solucion Optima")
-    b_barra = np.dot(B_inverse,b)
-    print("b_barra = ", b_barra)
-    z_result  = np.dot(pi_z,b)
-    print("z = ", z_result)
-    if z_result==0:
-        print("No existe solucion Factible, z = 0")
+    numciclo = 1
+    control = True
+
+    ##Ahora ejecutaremos ciclos hasta que todos los c_j sean mayores a ceros
+    ## o encontremos que no existe una SBF
+    while(not comprobar(c_barra) and control):
+        print("")
+        print("Ciclo numero ",numciclo)
+
+        ColumnaPivote = minCol(c_barra)
+        A_sBarra =np.dot(B_inverse,A[:, ColumnaPivote] )
+        FilaPivote = minFila(b,A_sBarra,minCol)
+        if(FilaPivote == "error"):
+            break
+        B_aux = replaceB(B_aux,ColumnaPivote,FilaPivote)
+        B = crearB(A,B_aux)
+        a_rs = A[FilaPivote][ColumnaPivote]
+        print("a_rs = ",a_rs)
+        P_s = createP_s(a_rs,A_sBarra,FilaPivote)
+        P = createP(P_s,FilaPivote)
+        B_inverse = createB_inverse(B_inverse,P)
+        c_B = crearc_B(c,B_aux)
+        pi_z = np.dot(c_B,B_inverse)
+        print("pi_z = ",pi_z)
+        c_barra = c - (np.dot(pi_z,A))
+        print("c_barra = ",c_barra)
+        numciclo = numciclo+1
+        print("")
+        print("")
+
+    # cumplida la condicion encontraremos la SBF
+    if(control == True):
+        print("Solucion Optima")
+        b_barra = np.dot(B_inverse,b)
+        print("b_barra = ", b_barra)
+        z_result  = np.dot(pi_z,b)
+        print("z = ", z_result)
+        if z_result==0:
+            print("No existe solucion Factible, z = 0")
+
+main()
 
 
 
